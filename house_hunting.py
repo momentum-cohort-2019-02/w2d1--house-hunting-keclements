464a9e8ff679c3b2282381c2b48f7a182a142fcd
keclements <kristin.clements@gmail.com>
2019-02-19 02:02:39 -0500

    f

--- house_hunting.py
+++ house_hunting.py
@@ -1,8 +1,9 @@
-# Program
 print("Welcome, let's find how long you need to save for your new home!")
-total_cost = int(input("How much is your dream home?"))
-# down_payment = int(inp)
-annual_salary = int(input("What is your annual salary?"))
-current_saved = int(input("How much have you already saved for your dream home?"))
+total_cost = int(input("How much is your dream home:"))
+annual_salary = int(input("What is your annual salary:"))
+current_saved = int(input("How much have you already saved for your dream home:"))
+percentage_down = total_cost * 0.25
 
-print("The total cost of your dream home is " + total_cost)
+print("Your total cost is ", total_cost)
+
+#print("To buy a house for ", total_cost, "with a salary of ", annual_salary, "and " current_saved, "you will need to put down", percentage_down)
