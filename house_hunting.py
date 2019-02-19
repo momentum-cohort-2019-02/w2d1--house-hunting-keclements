1c1789fe37d3ee70ace0a02f0f78a57ece892ec7
keclements <kristin.clements@gmail.com>
2019-02-19 02:21:47 -0500

    saving annual rate

--- house_hunting.py
+++ house_hunting.py
@@ -1,9 +1,18 @@
 print("Welcome, let's find how long you need to save for your new home!")
+
 total_cost = int(input("How much is your dream home:"))
 annual_salary = int(input("What is your annual salary:"))
-current_saved = int(input("How much have you already saved for your dream home:"))
-percentage_down = total_cost * 0.25
+current_savings = int(input("How much have you already saved for your dream home:"))
+percentage_down = float(total_cost * 0.25)
+current_savings = 0
+r = 0.04
+
 
 print("Your total cost is ", total_cost)
+print("The amount you need to put down is ", percentage_down)
+
+#print("You currently have " + current_savings)
+
+
 
 #print("To buy a house for ", total_cost, "with a salary of ", annual_salary, "and " current_saved, "you will need to put down", percentage_down)
