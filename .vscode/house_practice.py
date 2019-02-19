0049cbecc773ba81f6d7246a0d3ebb64f321c923
keclements <kristin.clements@gmail.com>
2019-02-19 10:05:45 -0500

    homework help after clas

---
+++ .vscode/house_practice.py
@@ -0,0 +1,48 @@
+# Test case:
+# Enter your annual salary: 120000
+# Enter the percent of your salary to save, as a decimal: .10
+# Enter the cost of your dream home: 1000000
+# Number of months: 183
+
+
+# get annual income
+annual_salary = int(input("Enter your annual salary:"))
+
+# figure out how much I want to save each month
+portion_saved = float(inpout("Enter the percentage of you salary to save as a decimal:"))
+
+# annual income monthly income
+monthly_salary= annual_salary / 12
+
+# get the price of the house and the percentage for the down payment
+portion_saved = float ()
+# Figure out how much I'm saving each month as a percentage of my income
+# each month, 
+amount_saved_each_month = portion_saved * monthly_salary
+total_cost = float)input("enter")
+#   caclculate the interest -- current savings * rate (0.04) / 12
+
+down_payment = total_cost + portion_saved
+#     add the amount I'm saving to a running total
+
+
+# Set initial information
+current_saving = 0
+current_month = 1
+interest_rate = 0.04
+
+# each month while our savings is < down payment
+while current_saving < down_payment:
+
+# calculate interest -- current saving * rate (0.04) / 12
+    interest = curren_savings * interest_rate / 12
+    # add interest to current saving
+    #current_saving - current_saving + interest
+    current_saving += interest #
+    # add the amount I'm sacing to my current saving
+# Report how many months that took
+
+
+#each month (amount of savings)* rate  
+
+# see the word each means yo have a loop
